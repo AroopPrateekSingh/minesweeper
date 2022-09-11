ec137c7839bd84c7afddc10a8d6def6a0b09b7d0
AroopPrateekSingh <113294022+AroopPrateekSingh@users.noreply.github.com>
2022-09-11 20:21:29 +0530

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,61 +1,29 @@
-"""
-Implementation of command-line minesweeper by Kylie Ying
-
-YouTube Kylie Ying: https://www.youtube.com/ycubed 
-Twitch KylieYing: https://www.twitch.tv/kylieying 
-Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
-Website: https://www.kylieying.com
-Github: https://www.github.com/kying18 
-Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
-
-Project specs, files, code all over the place? Start using Backlog for efficient management!! There is a free tier: https://cutt.ly/ehxImv5
-"""
-
 import random
 import re
 
-# lets create a board object to represent the minesweeper game
-# this is so that we can just say "create a new board object", or
-# "dig here", or "render this game for this object"
 class Board:
     def __init__(self, dim_size, num_bombs):
-        # let's keep track of these parameters. they'll be helpful later
+    
         self.dim_size = dim_size
         self.num_bombs = num_bombs
 
-        # let's create the board
-        # helper function!
-        self.board = self.make_new_board() # plant the bombs
+        self.board = self.make_new_board() 
         self.assign_values_to_board()
 
-        # initialize a set to keep track of which locations we've uncovered
-        # we'll save (row,col) tuples into this set 
-        self.dug = set() # if we dig at 0, 0, then self.dug = {(0,0)}
+        self.dug = set() 
 
     def make_new_board(self):
-        # construct a new board based on the dim size and num bombs
-        # we should construct the list of lists here (or whatever representation you prefer,
-        # but since we have a 2-D board, list of lists is most natural)
-
-        # generate a new board
+  
+       
         board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
-        # this creates an array like this:
-        # [[None, None, ..., None],
-        #  [None, None, ..., None],
-        #  [...                  ],
-        #  [None, None, ..., None]]
-        # we can see how this represents a board!
 
-        # plant the bombs
         bombs_planted = 0
         while bombs_planted < self.num_bombs:
-            loc = random.randint(0, self.dim_size**2 - 1) # return a random integer N such that a <= N <= b
-            row = loc // self.dim_size  # we want the number of times dim_size goes into loc to tell us what row to look at
-            col = loc % self.dim_size  # we want the remainder to tell us what index in that row to look at
+            loc = random.randint(0, self.dim_size**2 - 1) 
+            row = loc // self.dim_size  
+            col = loc % self.dim_size  
 
             if board[row][col] == '*':
-                # this means we've actually planted a bomb there already so keep going
                 continue
 
             board[row][col] = '*' # plant the bomb
@@ -64,13 +32,11 @@ class Board:
         return board
 
     def assign_values_to_board(self):
-        # now that we have the bombs planted, let's assign a number 0-8 for all the empty spaces, which
-        # represents how many neighboring bombs there are. we can precompute these and it'll save us some
-        # effort checking what's around the board later on :)
+     
         for r in range(self.dim_size):
             for c in range(self.dim_size):
                 if self.board[r][c] == '*':
-                    # if this is already a bomb, we don't want to calculate anything
+                   
                     continue
                 self.board[r][c] = self.get_num_neighboring_bombs(r, c)
 
